f29d139bfce9246fc63082e680b79bd480e30c4d
Param Kesarwani <20bcs150@iiitdmj.ac.in>
2021-10-20 12:48:12 +0530

    Added basic authentication

--- app.js
+++ app.js
@@ -1,83 +1,87 @@
 const express = require('express')
 const app = express()
-var bodyParser = require('body-parser');
+const session = require('express-session')
+const bodyParser = require('body-parser');
 const axios = require('axios')
 const mysql = require('mysql')
 const cors = require('cors');
 const connection = require('./db_service')  // database connection
-var http = require('http');
-var path = require('path');
-// var logger = require('morgan');
+const  http = require('http');
+const path = require('path');
+const admin = require('./admin') // for adding a new admin 
+const ip = require('./ip_address')  // for Ip address
+const speed = require('./speed')  // for internet speed test  
+app.set('view engine', 'ejs');
+
+app.use(session({
+  name:"sid",
+  secret:"key that will sign cookie",
+  resave:false,
+  saveUninitialized:false,
+  cookies : {
+    maxAge:1000*60*60*2,
+    sameSite:true,
+    secure:false
+  }
+}))
 
 app.use(cors({origin:'*'}));
-// app.use(logger('dev'));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('./public'))
+app.use('/admin',admin)  // use admin router
+app.use('/api/ip',ip)   // use ip router
+app.use('/api/speed',speed)  // use speed router
 
-app.get('/router',(req,res)=>{
-  // console.log(path.join(__dirname,'public','routerform.html'))
-  res.sendFile(path.join(__dirname,'public','routerform.html'))
+// 
+ 
+app.get('/ses',(req,res)=>{
+  console.log(__dirname);
+  res.render('/success.html');
 })
 
-app.post('/admin', function(req, res, next) {
-  var fname = req.body.fname;
-  var email = req.body.email;
-  var password = req.body.pass;
-  var date = req.body.date
-  var phone = req.body.phone;
-  var depart = req.body.department;
-    res.sendFile(path.join(__dirname,'public','success.html'));
- 
-  connection.query(`INSERT INTO admin (name,emailid, password,Date_on,phone_no) 
-  VALUES (?,?,?,?,?)`,[fname,email,password,date,phone], function(err, result) {
-    if (err) throw err;
-    console.log('record inserted');
+
+app.post('/login',(req,res)=>{
+  const {email,password }= req.body;
+     connection.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+      var x = result.find((user)=>{
+        return user.emailid === email && user.password === password;
+      })
+      if(x) return res.redirect('/user.html');
+      connection.query("SELECT * FROM admin", function (err, result, fields) {
+        if (err) throw err;
+        var y = result.find((admin)=>{
+          return admin.emailid === email && admin.password === password;
+        })
+        if(y) return res.redirect('/admin.html');
+        else return res.redirect('/login.html')
+
+      })
+
+
   });
 
-   for(x in depart){
-    connection.query(`INSERT INTO admin_dep (email,dep_head) 
-    VALUES (?,?)`,[email,depart[x]], function(err, result) {
-      if (err) throw err;
-      console.log('record inserted');
-    });
+  // res.send('got ur response')
+  // res.send(req.session);
+  // const {userId} = req.session
+  // console.log(userId);
 
-   }
-});
+})
 
 
 
+// app.post('/logout',(req,res)=>{
+//   req.session.destroy(err=>{
+//     if(err){
+//       return res.redirect('/home')
+//     }
+//     res.clearCookie('sid')
+//     res.redirect('/')
+//   })
 
-// app.get('/api/ip',(req,res)=>{
-//   http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-//     resp.on('data', function(ip) { ip = ''+ip;
-//       var date = new Date();
-//       var currdate = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
-//       var currtime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-//       connection.query(`insert into ip_address(ipaddress,Date_on,Time_on,Roll_no)
-//       VALUES (?,?,?,?)`,[ip,currdate,currtime,"20bcs150"], function (err, result) {
-//         if (err) throw err;
-//         console.log('done succesfully');
-//       });
-//       res.send(ip);
-//     });
-//   });
 // })
 
-
-
-
-// const FastSpeedtest = require("fast-speedtest-api");
- 
-// let speedtest = new FastSpeedtest({
-//     token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm", // required
-//     verbose: false, // default: false
-//     timeout: 10000, // default: 5000
-//     https: true, // default: true
-//     urlCount: 5, // default: 5
-//     bufferSize: 8, // default: 8
-//     unit: FastSpeedtest.UNITS.Mbps // default: Bps
-// });
 
 app.get('/dep',(req,res)=>{
   connection.connect(function(err) {
@@ -88,9 +92,5 @@ app.get('/dep',(req,res)=>{
   });
 })
  
-app.get('/api/speed',(req,res)=>{
-  speedtest.getSpeed().then(s => {  s=''+s;  res.send(s)
-  }).catch(e => { console.error(e.message);
-  });
-})
+
 app.listen(1337,()=>{console.log('server listen at port 3000')})
